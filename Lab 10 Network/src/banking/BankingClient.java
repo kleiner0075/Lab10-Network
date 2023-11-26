d4047cc5bcdd0e812f65ebbf41fb3846d134f7c7
1323a <1323a@192.168.40.25>
2023-11-25 19:25:16 -0500

    simple interface for bank

--- Lab 10 Network/src/banking/BankingClient.java
+++ Lab 10 Network/src/banking/BankingClient.java
@@ -1,11 +1,36 @@
 package banking;
 
+import java.util.Scanner;
+
 public class BankingClient {
 	public static void main(String[] args) {
 		//connect to server
 		//implement user interface
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Welcome to the Bank of RAK");
+		System.out.println("Please select an option:");
+		System.out.println("--------------------------");
+		System.out.println("1. Transfer Funds");
+		System.out.println("2. Check Balance");
+		System.out.println("3. Exit");
+
+		int numInput = userInput.nextInt();
+		switch(numInput) {
+		case 1:
+			System.out.println("how much would you like to transfer?");
+			break;
+		case 2:
+			System.out.println();
+			break;
+		case 3:
+			System.out.println("Goodbye!");
+			break;
+			default:
+				System.out.println("not a good answer");
+		}
+
 	}
-	
+
 	private void loginUser(String username, String password) {
 		//code for user validation
 		//references the db tables for column
@@ -15,13 +40,12 @@ public class BankingClient {
 		//code for funds transfer
 		//transfer funds from user 1 to user 2 or vice versa
 	}
-	
+
 	private double checkBalance(int userId) {
 		//fund
 		double funds = 0;
 		System.out.println("you're broke as fuck lmaooooooo");
-		System.out.println("ryan has tiny balls");
 
 		return funds;
-		}
+	}
 }
